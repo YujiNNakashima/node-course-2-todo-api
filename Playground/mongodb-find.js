32c009f016bdc9d55adeec2a521ca62829f64a21
Ricardo Yuji Nagamatsu Nakashima <ricardoyuji@brq.com>
2017-11-01 14:09:12 -0200

    add GET

--- Playground/mongodb-find.js
+++ Playground/mongodb-find.js
@@ -2,29 +2,29 @@
 const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-	if (err) {
-		return console.log("Unable to connect");
-	}
-	console.log('Connected to MongoDb server');
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-	// db.collection('Todos').find({
-	// 	_id: new ObjectID('59f73ecff346105bf4755f6b')
-	// }).toArray().then((docs) => {
-	// 	console.log('Todos');
-	// 	console.log(JSON.stringify(docs, undefined, 2));
-	// }, (err) => {
-	// 	console.log("unable to fetch todos", err);
-	// });
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('57bb36afb3b6a3801d8c479d')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
 
-	// db.collection('Todos').find().count().then((count) => {
-	// 	console.log(`Todos count: ${count}`);
-	// }, (err) => {
-	// 	console.log("unable to fetch todos", err);
-	// });
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
 
-	db.collection('Users').find({Name: 'yuji'}).toArray().then((docs) => {
-		console.log(JSON.stringify(docs, undefined, 2));
-	});
+  db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  });
 
-	// db.close();
-});;
+  // db.close();
+});
--- server/server.js
+++ server/server.js
@@ -1,30 +1,39 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose.js');
-//var mongoose = require(...).mongoose
-
+var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/user') 
+var {User} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-	var todo = new Todo({
-		text: req.body.text
-	});
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-	todo.save().then((doc) => {
-		res.send(doc);
-	}, (e) => {
-		res.status(400).send(e);
-	});
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 
-app.listen(3000, () => {
-	console.log('started on port 3000');
-})
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	})
+});
 
+
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+module.exports = {app};
