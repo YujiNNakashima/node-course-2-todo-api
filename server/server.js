60ea0b732ddfdf5525bde8da8d2879877cd92185
Ricardo Yuji Nagamatsu Nakashima <ricardoyuji@brq.com>
2017-11-03 13:00:48 -0200

    testes GET

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -22,45 +22,31 @@ app.post('/todos', (req, res) => {
   });
 });
 
-
 app.get('/todos', (req, res) => {
-	Todo.find().then((todos) => {
-		res.send({todos});
-	}, (e) => {
-		res.status(400).send(e);
-	})
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
-
-// GET / todos/12345535
-
-
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
-    console.log("ID not valid");
-  };
+  }
 
-  Todo.findById(id).then((certo)=>{
-    if(!certo) {
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
 
-    res.send({certo});
-
+    res.send({todo});
   }).catch((e) => {
-
     res.status(400).send();
-  
   });
-
-
-
-
 });
-
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
